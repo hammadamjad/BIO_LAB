3350b93443aa2d12dc6f37631edd441e7114cc52
HammadAmjad <hammadamjadali30@gmail.com>
2020-07-03 11:21:59 +0100

    3Ai 2d graph

--- 3D graph.py
+++ 3D graph.py
@@ -10,44 +10,27 @@ lst_z1 = []
 lst_z2 = []
 lst_z3 = []
 J=0
-e = 120
-while(J<9):
+e = 15
+while(J<10):
     lst_x.append(e)
-    e += 10
+    e += 2
     J += 1
-def func1(lst_z,y):
-    ex = 120
-    t = 0.1
+def func(lst_z,):
+    ex = 15
     i = 0
-    while(i<9):
-        tg = 387 +((-83.5*math.log10(1/y))/(32.58+math.log10(1/y)))
-        z = 1337*((1.42*tg+44.7)/(1.27*tg+0.3*ex))
+    while(i<10):
+        z = pow(1+0.64*0.0000012345*(pow(ex/100,2)/pow(1-(ex/100),2)),-0.5)
         lst_z.append(z)
-        ex+=10
-        i+=1
-    return lst_z
-def func2(lst_z,y):
-    ex = 120
-    t = 0.1
-    i = 0
-    while(i<9):
-        tg = 354 +((54*math.log10(1/y))/(3.4+math.log10(1/y)))
-        z = 1060*((1.42*tg+44.7)/(1.27*tg+0.3*ex))
-        lst_z.append(z)
-        ex+=10
+        ex+=2
         i+=1
     return lst_z
 
 x = lst_x
-z1 = func1(lst_z1,0.1)
-z2 = func2(lst_z2,0.1)
-# z3 = func(lst_z3,1)
-# plt.plot(x, z1,label = "STRAIN RATE = 0.1", marker='o',markersize=12, markerfacecolor='black',color='orange',linewidth=4)
-plt.plot(x, z2,label = "STRAIN RATE = 0.1", marker='o',markersize=12, markerfacecolor='black',color='red',linewidth=4)
-# plt.plot(x, z3,label = "NO. OF HOLES = 10", marker='o',markersize=12, markerfacecolor='black',color='blue',linewidth=4)
-plt.xlabel('TEMPERATURE ')
-plt.ylabel('DENSITY')
-plt.title("DENSITY ANALYSIS")
+z1 = func(lst_z1)
+plt.plot(x, z1,label = "DEPTH OF MOLD = 80mm", marker='o',markersize=12, markerfacecolor='black',color='orange',linewidth=4)
+plt.xlabel('RADIUS OF PLUG (rp)')
+plt.ylabel('STRETCH RATIO')
+plt.title("STRETCH ANALYSIS")
 ax.set_facecolor('yellow')
 plt.legend()
 plt.show()

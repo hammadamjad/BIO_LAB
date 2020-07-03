b1dd18d4823cae6bb4736e92fab8bb23fbe7ddd3
HammadAmjad <hammadamjadali30@gmail.com>
2020-07-03 08:41:19 +0100

    Q3D 2d graph

--- 3D graph.py
+++ 3D graph.py
@@ -4,35 +4,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ax = plt.axes()
-
+lst_x = []
 lst_z1 = []
 lst_z2 = []
 lst_z3 = []
-
+J=0
+e = 16887
+while(J<6):
+    lst_x.append(e)
+    e += 16887
+    J += 1
 def func(lst_z,y):
-    ex = 80
+    ex = 16887
+    t = 0.1
     i = 0
-    while(i<8):
-        C1 = (0.0003768*pow(ex,2))-(0.128*ex)-0.876
-        C2 = (0.00007053* pow(ex, 2)) + (0.018* ex) + 1.199
-        z1 = 2*C1*(y-(1/pow(y,4)))
-        z2 = 2*C2*(pow(y,3)-(1/pow(y,2)))
-        z = z1+z2
+    while(i<6):
+        z1 = 0.0027038/(t*y*3.14*pow(0.0015,2))
+        z2 = 1-pow(ex/101325,0.75187)
+        z = z1*z2
         lst_z.append(z)
-        ex+=10
+        ex+=16887
         i+=1
     return lst_z
 
-x = [80,90,100,110,120,130,140,150]
-z1 = func(lst_z1,1.8)
-z2 = func(lst_z2,2)
-z3 = func(lst_z3,2.4)
-plt.plot(x, z1,label = "STRETCH RATIO = 1.8", marker='o',markersize=12, markerfacecolor='black',color='orange',linewidth=4)
-plt.plot(x, z2,label = "STRETCH RATIO = 2", marker='o',markersize=12, markerfacecolor='black',color='red',linewidth=4)
-plt.plot(x, z3,label = "STRETCH RATIO = 2.4", marker='o',markersize=12, markerfacecolor='black',color='blue',linewidth=4)
-plt.xlabel('TEMPERATURE')
-plt.ylabel('TRUE STRESS')
-plt.title("STRESS ANALYSIS")
+x = lst_x
+z1 = func(lst_z1,2)
+z2 = func(lst_z2,5)
+z3 = func(lst_z3,10)
+plt.plot(x, z1,label = "NO. OF HOLES = 2", marker='o',markersize=12, markerfacecolor='black',color='orange',linewidth=4)
+plt.plot(x, z2,label = "NO. OF HOLES = 5", marker='o',markersize=12, markerfacecolor='black',color='red',linewidth=4)
+plt.plot(x, z3,label = "NO. OF HOLES = 10", marker='o',markersize=12, markerfacecolor='black',color='blue',linewidth=4)
+plt.xlabel('PRESSURE ')
+plt.ylabel('VELOCITY')
+plt.title("AIR EVACUATING VELOCITY")
 ax.set_facecolor('yellow')
 plt.legend()
 plt.show()

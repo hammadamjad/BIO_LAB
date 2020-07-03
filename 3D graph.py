22093aa668a1ce3e5fe792a686d64ebcb761f70f
HammadAmjad <hammadamjadali30@gmail.com>
2020-07-03 07:55:47 +0100

    1A changed to 2D graph

--- 3D graph.py
+++ 3D graph.py
@@ -3,50 +3,30 @@ from mplot3d_dragger import Dragger3D
 import numpy as np
 import matplotlib.pyplot as plt
 
-lst_y = []
-lst_n = []
+ax = plt.axes()
 
-fig = plt.figure()
-ax = Axes3D(fig)
+lst_z = []
+ex = 110
+y=0.3
+i=0
+while(i<10):
+    n = 575+(6.325*ex)-(0.0175*pow(ex,2))
+    E1 = 65.1 - (0.6675 * ex) + (0.00175 * pow(ex, 2))
+    E2 = 480 + (5.35 * ex) - (0.015 * pow(ex, 2))
+    expo = -n / (E2 * 0.1)
+    z = (E1 * y) + (E2 * 0.1) * (1 - np.exp(expo * y))
+    lst_z.append(z)
+    ex+=10
+    y+=0.20
+    i+=1
 
-def f(x, y):
-    E1 = np.exp(2*y)+(2*np.exp(-y))
-    E2 = np.exp(-2*y)+(2*np.exp(y))
-    # expo = -N/(E2*0.1)
-    z = (E1+E2-6)*0.635*0.25
-    return z
-
-# ex = 110
-# g = 12
-# t = 1
-# E = 250
-# i=0
-# while(i<10):
-#     n = 575+(6.325*ex)-(0.0175*pow(ex,2))
-#     ey = (g/E)*(1-np.exp(-E/(n*t)))
-#     lst_y.append(ey)
-#     lst_n.append(n)
-#     ex+=10
-#     g+=2
-#     t+=1000
-#     E+=40
-#     i+=1
-# len_y = len(lst_y)-1
-# len_n = len(lst_n)-1
-x = np.linspace(100, 180, 30)
-y = np.linspace(0.1,2.5, 30)
-# N = np.linspace(lst_n[0],lst_n[len_n],30)
-
-X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
-
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1,cmap='viridis', edgecolor='none')
-ax.set_title('STRAIN ENERGY ANALYSIS')
-surf1 = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,cmap='viridis', edgecolor='none')
-fig.colorbar(surf1, ax=ax, shrink=0.5, aspect=5)
-ax.set_xlabel("TEMPERATURE")
-ax.set_ylabel("STRAIN")
-ax.set_zlabel("STRAIN ENERGY")
-dr = Dragger3D(ax)
+len_z = len(lst_z)-1
+x = [110,120,130,140,150,160,170,180,190,200]
+z = lst_z
+plt.plot(x, z,label = "STRAIN RATE = 0.1", marker='o',markersize=12, markerfacecolor='black',color='orange',linewidth=4)
+plt.xlabel('TEMPERATURE')
+plt.ylabel('TRUE STRESS')
+plt.title("STRESS ANALYSIS")
+ax.set_facecolor('yellow')
+plt.legend()
 plt.show()

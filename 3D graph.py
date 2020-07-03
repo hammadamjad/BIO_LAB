fd6a58eefaf61e8d52a2081cf2e22067764527b1
HammadAmjad <hammadamjadali30@gmail.com>
2020-07-03 11:32:21 +0100

    3Aii 2D graph

--- 3D graph.py
+++ 3D graph.py
@@ -7,30 +7,31 @@ import matplotlib.pyplot as plt
 ax = plt.axes()
 lst_x = []
 lst_z1 = []
-lst_z2 = []
-lst_z3 = []
 J=0
-e = 15
+e = 0
 while(J<10):
-    lst_x.append(e)
-    e += 2
+    H = e/254
+    lst_x.append(H)
+    e += 16
     J += 1
 def func(lst_z,):
-    ex = 15
+    ex = 0
     i = 0
     while(i<10):
-        z = pow(1+0.64*0.0000012345*(pow(ex/100,2)/pow(1-(ex/100),2)),-0.5)
+        num = (508*ex)/(64516+pow(ex,2))
+        dem = np.arcsin(num)
+        z = num/dem
         lst_z.append(z)
-        ex+=2
+        ex+=16
         i+=1
     return lst_z
 
 x = lst_x
 z1 = func(lst_z1)
-plt.plot(x, z1,label = "DEPTH OF MOLD = 80mm", marker='o',markersize=12, markerfacecolor='black',color='orange',linewidth=4)
-plt.xlabel('RADIUS OF PLUG (rp)')
-plt.ylabel('STRETCH RATIO')
-plt.title("STRETCH ANALYSIS")
+plt.plot(x, z1,label = "HALF WIDTH = 254mm", marker='o',markersize=12, markerfacecolor='black',color='orange',linewidth=4)
+plt.xlabel('H/a')
+plt.ylabel('t/to')
+plt.title("THICKNESS VARIATION")
 ax.set_facecolor('yellow')
 plt.legend()
 plt.show()
